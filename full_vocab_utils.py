fix: Epoch accuracy calculation

full_vocab_utils.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 import torch
 from torch import nn
+import numpy as np
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
@@ -13,10 +14,10 @@ loss_fn = nn.CrossEntropyLoss().to(device)
 
 @timeit
 @torch.no_grad()
-def evaluate(model, dataloader, loss_func, read_prev=False):
+def evaluate(model, dataloader, loss_func, pad_idx, read_prev=False):
     model.eval()
 
-    total_loss = total_acc = sample_count = 0
+    epoch_loss = epoch_acc = total_count = non_pad_count = 0
     wrong_preds = Counter()
     for inputs, targets, prevs, _ in dataloader:
         inputs, targets, prevs = inputs.to(device), targets.to(device), prevs.to(device)
@@ -26,24 +27,26 @@ def evaluate(model, dataloader, loss_func, read_prev=False):
         targets = targets.view(-1)
 
         loss = loss_func(outputs, targets)
-        preds = outputs.argmax(1).cpu().numpy()
-        acc = accuracy_score(preds, targets.cpu())
-        wrong_preds += Counter([(y_hat, y) for y_hat, y in zip(preds, targets.tolist()) if y_hat != y])
+        y = np.vstack([outputs.argmax(1).cpu().numpy(), targets.cpu().numpy()])  # 2, seq_len * batch_size
+        y = y[:, np.where(y[1, :] != pad_idx)[0]]  # 2, filtered_non_padding
+        acc = accuracy_score(y[0], y[1])
+        mismatches = y[:, np.where(y[0, :] != y[1, :])[0]]  # 2, filtered_wrong_prediction
+        wrong_preds += Counter(map(tuple, mismatches.T))
 
-        count = len(targets)
-        sample_count += count
-        total_loss += loss.item() * count
-        total_acc += acc * count
+        total_count += len(targets)  # seq_len * batch_size
+        epoch_loss += loss.item() * len(targets)
+        non_pad_count += len(y[1])  # filtered_non_padding
+        epoch_acc += acc * len(y[1])
 
     model.train()
-    return total_loss / sample_count, total_acc / sample_count, wrong_preds
+    return epoch_loss / total_count, epoch_acc / non_pad_count, wrong_preds
 
 
 @timeit
-def train_1_epoch(model, dataloader, loss_func, optimizer, read_prev=False):
+def train_1_epoch(model, dataloader, loss_func, optimizer, pad_idx, read_prev=False):
     model.train()
 
-    total_loss = total_acc = sample_count = 0
+    epoch_loss = epoch_acc = total_count = non_pad_count = 0
     for inputs, targets, prevs, _ in dataloader:
         inputs, targets, prevs = inputs.to(device), targets.to(device), prevs.to(device)
         optimizer.zero_grad()
@@ -53,21 +56,22 @@ def train_1_epoch(model, dataloader, loss_func, optimizer, read_prev=False):
         targets = targets.view(-1)
 
         loss = loss_func(outputs, targets)
-        preds = outputs.argmax(1)
-        acc = accuracy_score(preds.cpu(), targets.cpu())
+        y = np.vstack([outputs.argmax(1).cpu().numpy(), targets.cpu().numpy()])  # 2, seq_len * batch_size
+        y = y[:, np.where(y[1, :] != pad_idx)[0]]  # 2, filtered_non_padding
+        acc = accuracy_score(y[0], y[1])
 
         loss.backward()
         optimizer.step()
 
-        count = len(targets)
-        sample_count += count
-        total_loss += loss.item() * count
-        total_acc += acc * count
+        total_count += len(targets)  # seq_len * batch_size
+        epoch_loss += loss.item() * len(targets)
+        non_pad_count += len(y[1])  # filtered_non_padding
+        epoch_acc += acc * len(y[1])
 
-    return total_loss / sample_count, total_acc / sample_count
+    return epoch_loss / total_count, epoch_acc / non_pad_count
 
 
-def train_model(model, filename, train_loader, val_loader, optim, lr=1e-3, epochs=10, read_prev=False):
+def train_model(model, filename, train_loader, val_loader, pad_idx, optim, lr=1e-3, epochs=10, read_prev=False):
     model.to(device)
     optimizer = optim(model.parameters(), lr=lr)
     min_val_loss = float('inf')
@@ -76,10 +80,10 @@ def train_model(model, filename, train_loader, val_loader, optim, lr=1e-3, epoch
     for epoch in range(epochs):
         print(f'Epoch: {epoch + 1: 02}')
 
-        train_loss, train_acc = train_1_epoch(model, train_loader, loss_fn, optimizer, read_prev=read_prev)
+        train_loss, train_acc = train_1_epoch(model, train_loader, loss_fn, optimizer, pad_idx, read_prev=read_prev)
         print(f'\tTrain Loss: {train_loss: .3f} | Train Acc: {train_acc * 100: .2f}%')
 
-        val_loss, val_acc, wrong_preds = evaluate(model, val_loader, loss_fn, read_prev=read_prev)
+        val_loss, val_acc, wrong_preds = evaluate(model, val_loader, loss_fn, pad_idx, read_prev=read_prev)
         print(f'\t Val. Loss: {val_loss: .3f} |  Val. Acc: {val_acc * 100: .2f}%')
 
         if val_loss < min_val_loss:

neg_sample_utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import numpy as np
 from sklearn.metrics import accuracy_score
 
 from utils import timeit
@@ -46,33 +47,36 @@ def get_predictions(vocab_embed, hidden):
 
 @timeit
 @torch.no_grad()
-def evaluate(model, dataloader, vocab_tensor):
+def evaluate(model, dataloader, pad_idx, vocab_tensor):
     model.eval()
 
-    total_loss = total_acc = sample_count = 0
+    epoch_loss = epoch_acc = total_count = non_pad_count = 0
     for inputs, targets, _, neg_samples in dataloader:
         inputs, targets, neg_samples = inputs.to(device), targets.to(device), neg_samples.to(device)
 
         neg_embed, hidden, pos_embed = model(neg_samples, inputs, targets)
         loss = loss_fn(neg_embed, hidden, pos_embed)
         vocab_embed = model(vocab_tensor)[0]
-        preds = get_predictions(vocab_embed, hidden).cpu()
-        acc = accuracy_score(preds, targets.view(-1).cpu())
+        targets = targets.view(-1)
+        y = np.vstack([get_predictions(vocab_embed, hidden).cpu().numpy(), targets.cpu().numpy()])
+        # 2, seq_len * batch_size
+        y = y[:, np.where(y[1, :] != pad_idx)[0]]  # 2, filtered_non_padding
+        acc = accuracy_score(y[0], y[1])
 
-        count = preds.shape[0]
-        sample_count += count
-        total_loss += loss.item() * count
-        total_acc += acc * count
+        total_count += len(targets)  # seq_len * batch_size
+        epoch_loss += loss.item() * len(targets)
+        non_pad_count += len(y[1])  # filtered_non_padding
+        epoch_acc += acc * len(y[1])
 
     model.train()
-    return total_loss / sample_count, total_acc / sample_count
+    return epoch_loss / total_count, epoch_acc / non_pad_count
 
 
 @timeit
-def train_1_epoch(model, dataloader, optimizer, vocab_tensor):
+def train_1_epoch(model, dataloader, optimizer, pad_idx, vocab_tensor):
     model.train()
 
-    total_loss = total_acc = sample_count = 0
+    epoch_loss = epoch_acc = total_count = non_pad_count = 0
     for inputs, targets, _, neg_samples in dataloader:
         inputs, targets, neg_samples = inputs.to(device), targets.to(device), neg_samples.to(device)
         optimizer.zero_grad()
@@ -80,21 +84,24 @@ def train_1_epoch(model, dataloader, optimizer, vocab_tensor):
         neg_embed, hidden, pos_embed = model(neg_samples, inputs, targets)
         loss = loss_fn(neg_embed, hidden, pos_embed)
         vocab_embed = model(vocab_tensor)[0]
-        preds = get_predictions(vocab_embed, hidden).cpu()
-        acc = accuracy_score(preds, targets.view(-1).cpu())
+        targets = targets.view(-1)
+        y = np.vstack([get_predictions(vocab_embed, hidden).cpu().numpy(), targets.cpu().numpy()])
+        # 2, seq_len * batch_size
+        y = y[:, np.where(y[1, :] != pad_idx)[0]]  # 2, filtered_non_padding
+        acc = accuracy_score(y[0], y[1])
 
         loss.backward()
         optimizer.step()
 
-        count = preds.shape[0]
-        sample_count += count
-        total_loss += loss.item() * count
-        total_acc += acc * count
+        total_count += len(targets)  # seq_len * batch_size
+        epoch_loss += loss.item() * len(targets)
+        non_pad_count += len(y[1])  # filtered_non_padding
+        epoch_acc += acc * len(y[1])
 
-    return total_loss / sample_count, total_acc / sample_count
+    return epoch_loss / total_count, epoch_acc / non_pad_count
 
 
-def train_model(model, filename, train_loader, val_loader, vocab_size, optim, lr=1e-3, epochs=20):
+def train_model(model, filename, train_loader, val_loader, vocab_size, pad_idx, optim, lr=1e-3, epochs=20):
     model.to(device)
     optimizer = optim(model.parameters(), lr=lr)
     vocab_tensor = torch.arange(vocab_size).to(device)
@@ -103,10 +110,10 @@ def train_model(model, filename, train_loader, val_loader, vocab_size, optim, lr
     for epoch in range(epochs):
         print(f'Epoch: {epoch + 1: 02}')
 
-        train_loss, train_acc = train_1_epoch(model, train_loader, optimizer, vocab_tensor)
+        train_loss, train_acc = train_1_epoch(model, train_loader, optimizer, pad_idx, vocab_tensor)
         print(f'\tTrain Loss: {train_loss: .3f} | Train Acc: {train_acc * 100: .2f}%')
 
-        val_loss, val_acc = evaluate(model, val_loader, vocab_tensor)
+        val_loss, val_acc = evaluate(model, val_loader, pad_idx, vocab_tensor)
         print(f'\t Val. Loss: {val_loss: .3f} |  Val. Acc: {val_acc * 100: .2f}%')
 
         if val_loss < min_val_loss: