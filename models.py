b15c2198561ff247afe24b42f2b9fdc8a67e0f7e
adengz <adengz@gmail.com>
2020-12-20 12:50:35 +0800

    refactor: Standardize negative sampling model

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from typing import Optional, Union, Tuple
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from data import Vocabulary
@@ -76,7 +77,7 @@ class FullVocabularyModel(nn.Module):
             kwargs: Unused kwargs from DataLoader.
 
         Returns:
-            (seq_len * batch_size, vocab_size), hidden states
+            Output with loss, filtered predictions and ground_truth
         """
         hidden = None
         if self.read_context:
@@ -95,69 +96,103 @@ class FullVocabularyModel(nn.Module):
         return Output(loss, predictions, ground_truth)
 
 
-class NegSampleModel(nn.Module):
+class NegativeSamplingModel(nn.Module):
 
-    def __init__(self, vocab_size: int, embedding_dim: int, padding_idx: int,
-                 embed_dropout: float = 0.5, rnn_dropout: float = 0.5):
+    def __init__(self, vocab_size: int, embedding_dim: int, dropout: float = 0.2, pad_idx: int = Vocabulary.pad_idx):
         """
         Model trained with negative sampling.
 
         Args:
             vocab_size: Vocabulary size.
             embedding_dim: Word embedding dimension.
-            padding_idx: Index of padding token in vocabulary.
-            embed_dropout: Embedding dropout. Default: 0.5
-            rnn_dropout: RNN dropout. Default: 0.5
+            dropout: Dropout applied on embedding. Default: 0.2
+            pad_idx: Index of padding token in vocabulary.
+                Default: Vocabulary.pad_idx
         """
-        super(NegSampleModel, self).__init__()
-        self.in_embed = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
-        self.out_embed = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
-        self.embed_dropout = nn.Dropout(embed_dropout)
+        super(NegativeSamplingModel, self).__init__()
+        self.in_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.out_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.embed_dropout = nn.Dropout(dropout)
         self.rnn = nn.LSTM(embedding_dim, embedding_dim)
-        self.rnn_dropout = nn.Dropout(rnn_dropout)
         self.embedding_dim = embedding_dim
+        self.pad_idx = pad_idx
 
         initrange = 0.5 / embedding_dim
-        self.in_embed.weight.data.uniform_(-initrange, initrange)
-        self.in_embed.weight.data[padding_idx].zero_()
-        self.out_embed.weight.data.uniform_(-initrange, initrange)
-        self.out_embed.weight.data[padding_idx].zero_()
+        self.in_embedding.weight.data.uniform_(-initrange, initrange)
+        self.in_embedding.weight.data[pad_idx].zero_()
+        self.out_embedding.weight.data.uniform_(-initrange, initrange)
+        self.out_embedding.weight.data[pad_idx].zero_()
 
-    def forward(self, samples: torch.Tensor, text: torch.Tensor = None, targets: torch.Tensor = None) \
-            -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def forward(self, input_encoded: torch.LongTensor, target_encoded: torch.LongTensor,
+                input_lengths: torch.LongTensor, neg_samples: torch.LongTensor, **kwargs) \
+            -> Output:
         """
 
         Args:
-            samples: seq_len, batch_size, sample_size
-                or vocab_size
-            text: seq_len, batch_size
-                (Default to None when only embedding vocabulary)
-            targets: seq_len, batch_size
-                (Default to None when only embedding vocabulary)
+            input_encoded: batch_size, pad_len
+            target_encoded: batch_size, pad_len
+            input_lengths: batch_size
+            neg_samples: batch_size, pad_len, neg_count
+            kwargs: Unused kwargs from DataLoader.
 
         Returns:
-            (seq_len * batch_size, sample_size, embedding_dim),
-            (seq_len * batch_size, embedding_dim, 1)
-                or None when only embedding vocabulary,
-            (seq_len * batch_size, 1, embedding_dim)
-                or None when only embedding vocabulary
+            Output with loss, filtered predictions and ground_truth
         """
-        sample_size = samples.shape[-1]
-        samples_embedded = self.embed_dropout(self.out_embed(samples))
-        # seq_len, batch_size, sample_size, embedding_dim
-        samples_embedded = samples_embedded.view(-1, sample_size, self.embedding_dim)
-        # seq_len * batch_size, sample_size, embedding_dim
+        input_embedded = self.embed_dropout(self.in_embedding(input_encoded))  # batch_size, pad_len, embedding_dim
+        hidden = _feed_rnn(self.rnn, input_embedded, input_lengths)[0]  # batch_size, pad_len, embedding_dim
+        hidden = hidden.view(-1, self.embedding_dim)  # batch_size * pad_len, embedding_dim
 
-        rnn_output = None
-        if text is not None:
-            text_embedded = self.embed_dropout(self.in_embed(text))  # seq_len, batch_size, embedding_dim
-            rnn_output = self.rnn_dropout(self.rnn(text_embedded)[0])  # seq_len, batch_size, embedding_dim
-            rnn_output = rnn_output.view(-1, self.embedding_dim).unsqueeze(2)  # seq_len * batch_size, embedding_dim, 1
+        target_encoded = target_encoded.view(-1)  # batch_size * pad_len
+        pos_embedded = self.embed_dropout(self.out_embedding(target_encoded))  # batch_size * pad_len, embedding_dim
+        neg_count = neg_samples.shape[-1]
+        neg_embedded = self.embed_dropout(self.out_embedding(neg_samples)).view(-1, neg_count, self.embedding_dim)
+        # batch_size * pad_len, neg_count, embedding_dim
 
-        targets_embedded = None
-        if targets is not None:
-            targets_embedded = self.embed_dropout(self.out_embed(targets))  # seq_len, batch_size, embedding_dim
-            targets_embedded = targets_embedded.view(-1, self.embedding_dim).unsqueeze(1)
-            # seq_len * batch_size, 1, embedding_dim
+        mask = (input_encoded != self.pad_idx).view(-1)
+        loss = self.loss_fn(hidden, pos_embedded, neg_embedded, mask)
+        vocab_embedded = self.embed_dropout(self.out_embedding.weight.data)  # vocab_size, embedding_dim
+        vocab_embedded, _ = torch.broadcast_tensors(vocab_embedded[None, :, :], torch.Tensor(hidden.shape[0], 1, 1))
+        # batch_size * pad_len, vocab_size, embedding_dim
+        score = self.get_score(vocab_embedded, hidden)  # batch_size * pad_len, vocab_size
+        predictions = score.argmax(dim=-1).masked_select(mask)
+        ground_truth = target_encoded.masked_select(mask)
 
-        return samples_embedded, rnn_output, targets_embedded
+        return Output(loss, predictions, ground_truth)
+
+    @staticmethod
+    def get_score(embedded: torch.Tensor, hidden: torch.Tensor) -> torch.Tensor:
+        """
+        Calculates score.
+
+        Args:
+            embedded: batch_size * pad_len, sample_count, embedding_dim
+            hidden: batch_size * pad_len, embedding_dim
+
+        Returns:
+            batch_size * pad_len, sample_count
+                (squeezed if sample_count=1)
+        """
+        return torch.bmm(embedded, hidden[:, :, None]).squeeze()
+
+    def loss_fn(self, hidden: torch.Tensor, pos_embedded: torch.Tensor, neg_embedded: torch.Tensor,
+                mask: torch.BoolTensor) -> torch.Tensor:
+        """
+        Negative sampling loss function.
+
+        Args:
+            hidden: batch_size * pad_len, embedding_dim
+            pos_embedded: batch_size * pad_len, embedding_dim
+            neg_embedded: batch_size * pad_len, neg_count, embedding_dim
+            mask: batch_size * pad_len
+
+        Returns:
+            0-dim loss
+        """
+        pos_embedded = pos_embedded[:, None, :]  # batch_size * pad_len, 1, embedding_dim
+
+        pos_score = self.get_score(pos_embedded, hidden)  # batch_size * pad_len
+        pos_contrib = -F.logsigmoid(pos_score)
+        neg_score = self.get_score(neg_embedded, hidden)  # batch_size * pad_len, neg_count
+        neg_contrib = -torch.log(1 - torch.sigmoid(neg_score)).mean(dim=-1)  # batch_size * pad_len
+
+        return torch.masked_select(pos_contrib + neg_contrib, mask).mean()
